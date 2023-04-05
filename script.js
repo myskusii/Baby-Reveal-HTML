423a3f9fb62e069c41d5dc86845d87d861e56bc8
João Daniel Ribeiro <mks.joao@gmail.com>
2023-04-05 12:22:56 -0300

    Agora vai

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
 let mid = document.querySelector(".mid");
-let number = 300;
+let number = 18000;
 
 let flash = document.querySelector(".container div");
 
 btn.onclick = function () {
   container.style.transform = "rotate(" + number + "deg)";
+  number += 18000;
 }
 
 mid.onclick = function() {
   container.style.transform = "rotate(" + number + "deg)";
+  number += 18000;
 }
+
