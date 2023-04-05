d8d3b668cb94ee8bb86f858b78096ad85d08fb94
João Daniel Ribeiro <mks.joao@gmail.com>
2023-04-05 12:00:07 -0300

    Girl

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
 let mid = document.querySelector(".mid");
-let number = 240;
+let number = 300;
 
 let flash = document.querySelector(".container div");
 
 btn.onclick = function () {
   container.style.transform = "rotate(" + number + "deg)";
-  number += Math.ceil(Math.random() * 10000);
 }
 
 mid.onclick = function() {
   container.style.transform = "rotate(" + number + "deg)";
-  number += Math.ceil(Math.random() * 10000);
 }
-
