8899917e36d68a2289bdd6c2cd28649cbf55f693
João Daniel Ribeiro <mks.joao@gmail.com>
2023-04-05 11:49:11 -0300

    É uma menina!

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
 let mid = document.querySelector(".mid");
-let number = Math.ceil(Math.random() * 10000);
+let number = 2400;
 
 let flash = document.querySelector(".container div");
 
